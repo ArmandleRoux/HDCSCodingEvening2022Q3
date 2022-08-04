31d15f59dd32aef84fb89168dcbd05d387938132
Armand <101796497+ArmandleRoux@users.noreply.github.com>
2022-08-04 18:45:20 +0200

    Initial commit

---
+++ all_about_strings.py
@@ -0,0 +1,19 @@
+def allAboutStrings(word):
+    if len(word) > 2:
+        length = len(word)
+        first = word[0]
+        last = word[-1]
+        if len(word) % 2 > 0:
+            middle = word[int(len(word)/2)]
+        else:
+            middle = word[int(len(word)/2)-1:int(len(word)/2)+1]
+        if word[1] in word[2:]:
+            index = "@ index " + str(word[2:].index(word[1]) + 2)
+        else:
+            index = "not found"
+        return [length, first, last, middle, index]
+    return []
+
+print(allAboutStrings("LASA"))
+print(allAboutStrings("Computer"))
+print(allAboutStrings("Science"))
