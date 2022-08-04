614d073b20cacb7e4e334a751a130374555d267e
Armand <101796497+ArmandleRoux@users.noreply.github.com>
2022-08-04 19:08:54 +0200

    Name changes

--- all_about_strings.py
+++ all_about_strings.py
@@ -1,18 +1,18 @@
 def all_about_strings(word):
     if len(word) > 2:
-        length = len(word)
-        first = word[0]
-        last = word[-1]
+        word_length = len(word)
+        first_letter = word[0]
+        last_letter = word[-1]
         # Check if word length is even or odd to determine
         # middle
         if len(word) % 2 > 0:
-            middle = word[int(len(word)/2)]
+            middle_letter = word[int(len(word)/2)]
         else:
-            middle = word[int(len(word)/2)-1:int(len(word)/2)+1]
+            middle_letter = word[int(len(word)/2)-1:int(len(word)/2)+1]
         # Check if second character is in the rest of the string
         if word[1] in word[2:]:
             index = "@ index " + str(word[2:].index(word[1]) + 2)
         else:
             index = "not found"
-        return [length, first, last, middle, index]
+        return [word_length, first_letter, last_letter, middle_letter, index]
     return []
